c1bcc54c9eb4143685c2363a3fafa5fd937b1409
HussienSaeed1 <hussien_mahmoud@bydotpy.com>
2026-05-02 21:55:45 +0300

    privacy-policy screen

---
+++ src/app/[locale]/privacy-policy/page.js
@@ -0,0 +1,163 @@
+export default function PrivacyPolicyPage() {
+  return (
+    <div  dir="ltr" className="privacy-policy-container">
+      <h1>PRIVACY POLICY - FRAKTO for Real Estate Investment LLC</h1>
+
+      <h2>Introduction</h2>
+      <p>FRAKTO for Real Estate Investment LLC is committed to ensuring the utmost privacy and security of our customers personal information. This policy outlines how we collect, use, and disclose such information. Personal information encompasses all data, both directly and indirectly obtained (including via cookies or similar tools), regarding you, your transactions, financial details, interactions with us, and information received from third parties or gathered through our websites and mobile solutions.</p>
+
+      <p>If you have any queries regarding this privacy policy or the security of your personal data, please contact us at privacy@frakt.com. We encourage you to carefully read this Privacy Policy before opening an account with us, as your consent to our data practices is essential in accordance with the Terms and Conditions and this Privacy Policy. Through the implementation of this policy, we aim to provide you with a secure and enjoyable online experience.</p>
+
+      <p>By visiting our website, you accept and consent to the practices outlined in this Privacy Policy. Please note that your use of Platform is subject to our Terms of Use, and any products or services provided are governed by our Terms and Conditions.</p>
+
+      <h2>1. What is the purpose of a privacy policy?</h2>
+      <p>Privacy laws establish vital standards concerning information that identifies a living individual, known as &quot;personal data.&quot; It is mandatory for all organizations handling personal data to do so fairly and lawfully. At our company, we take our obligations seriously and ensure compliance when storing and processing your personal data. Our primary focus is to collect, store, and process only the necessary amount of personal data required to deliver and perform our services. This may include typical details such as:</p>
+
+      <ul>
+        <li><strong>Personal Information:</strong> We may collect National ID Copy, Passport Copy, personal information such as your name, email address, contact details, Anti Money Laundering/Know Your Customer (AML/KYC) related information, and other relevant information when you register for an account or interact with our services.</li>
+        <li><strong>Financial Information:</strong> To facilitate transactions on our platform, we may collect financial information, including bank account details and payment information.</li>
+        <li><strong>Usage Data:</strong> We gather data on how you interact with FRAKTO Website, including your IP address, browser type, device information, and browsing patterns.</li>
+        <li><strong>Cookies and Similar Technologies:</strong> Our website uses cookies and similar technologies to enhance your user experience. You can control cookie preferences through your browser settings.</li>
+      </ul>
+
+      <h2>2. How is your personal data collected?</h2>
+      <p>We employ various methods to collect your personal data, which include the following:</p>
+      <ul>
+        <li>When you provide it during communication with us, such as when registering for our services.</li>
+        <li>When you place any customer request in our platform.</li>
+        <li>When you participate in a competition or promotion.</li>
+        <li>When you make payments or modify your account details, request any withdrawal or deposit into your balance at our platform.</li>
+        <li>When you visit our website, information is collected through cookies and technical data, including your Internet protocol (IP) address used to connect your computer to the internet, login details, browser type and version, time zone setting, browser plug-in types and versions, operating system and platform, and other browser-generated information. This may also include details about your visit, such as the full Uniform Resource Locators (URL), clickstream data to, through, and from our site (including date and time), products viewed or searched for, page response times, download errors, length of visits to certain pages, page interaction information (such as scrolling, clicks, and mouse-overs), and the methods used to browse away from the page, along with any phone number used to call our customer service number.</li>
+      </ul>
+
+      <h2>3. IP Addresses and Cookies</h2>
+
+      <h3>3.1. Address Collection</h3>
+      <p>Whenever applicable, information about your computer such as your browser type, operating system and IP address, may be collected for system administration and to report aggregate information to our advertisers. The data collected is statistical with regards to our users browsing actions and patterns, and does not identify any individual.</p>
+
+      <h3>3.2. Cookie Usage</h3>
+      <p>We might use a cookie file for the same reason. The cookie file is saved on your computer&apos;s hard drive and collects information which, in turn, is transferred to your computer&apos;s hard drive. It helps us learn more about your general internet usage. This helps us improve our site and deliver a better and more personalized service. Cookies enable us to:</p>
+      <ul>
+        <li>Estimate the size and usage pattern of our audience</li>
+        <li>Store information about your preferences, and so allow us to tailor the Platform experience according to your individual interests</li>
+        <li>Speed up your searches</li>
+        <li>Recognize you when you return to the Platform</li>
+      </ul>
+      <p>Our advertisers may also use cookies, over which we have no control. For further information, please contact those advertisers regarding their own privacy policies.</p>
+
+      <h2>4. How do we use your personal data?</h2>
+      <p>We retain and use your personal data for the following main purposes:</p>
+      <ul>
+        <li>Assessing customer suitability for products and services</li>
+        <li>Conducting Anti-Money Laundering (AML) and Know Your Customer (KYC) assessments, and establishing investor risk profiles</li>
+        <li>Conducting market research and surveys to enhance our products and services</li>
+        <li>Preventing, detecting, investigating, and prosecuting crimes like money laundering, terrorism, fraud, and other financial crimes across jurisdictions, including identity verification, government sanctions screening, and due diligence checks</li>
+        <li>Complying with local and foreign laws, regulations, codes, directives, court orders, agreements with authorities, and reporting requirements under financial transaction legislation</li>
+        <li>Seeking professional advice, including legal proceedings and establishing legal rights</li>
+        <li>Enforcing our Terms and Conditions and complying with legal obligations</li>
+        <li>Processing applications, managing accounts, providing services, and communication purposes</li>
+        <li>Servicing our relationship with you, including administration, accounting, billing, and auditing</li>
+        <li>Ensuring security, payment verification, preventing fraud and money laundering, and debt recovery</li>
+        <li>Generating anonymous statistics on user behavior and website traffic. When we do this, data is anonymized and you are not personally identifiable</li>
+        <li>Providing information about goods or services that may interest you from us or selected third parties</li>
+        <li>Notifying you about changes to our services</li>
+        <li>Presenting website content effectively for you and for your computer</li>
+        <li>Administering and troubleshooting our website and conducting data analysis, research, and surveys</li>
+        <li>Allowing you to participate in interactive features of our service, when you choose to do so</li>
+        <li>Maintaining website security</li>
+        <li>Measuring advertising effectiveness and delivering relevant advertising to you</li>
+        <li>Making suggestions and recommendations to you and other users of our website about goods or services that may interest you or them</li>
+        <li>Except where you have opted out, we may also use your personal data to keep you informed about relevant products and services</li>
+      </ul>
+      <p>Your privacy is vital to us, and we are committed to using your personal data responsibly and securely.</p>
+
+      <h2>5. How do we protect your personal data?</h2>
+      <p>The security and confidentiality of your personal information are of utmost importance to us. We have allocated significant resources to safeguard and maintain the privacy of your data. When engaging external service providers, we ensure that they adhere to the same stringent standards as us. It is possible that your personal information may be transferred or stored outside your country of residence. Irrespective of the location of transfer or storage, we take all reasonable measures to ensure the security of your personal information.</p>
+
+      <p>It is essential to note that the Internet is not entirely secure, and transmitting information over it entails certain risks, including potential access and interference by unauthorized third parties. Information transmitted via the Internet may cross international borders, exposing it to jurisdictions with less robust privacy and data protection laws than your country of residence. We cannot be held liable for the confidentiality, security, or integrity of your personal information during its transmission over the Internet.</p>
+
+      <p>Nevertheless, we exercise utmost care in collecting, storing, processing, and disclosing your personal data. To minimize unauthorized access, we have implemented internal security procedures. As an additional layer of protection, we may request proof of identity before disclosing any personal information.</p>
+
+      <p>To enhance the protection of your personal data and reduce the risk of interception by unauthorized parties, our secure servers utilize industry-standard Secure Socket Layer (SSL) and Transport Layer Security (TLS) technology when you submit information through our website. This security is indicated by the use of the &quot;https&quot; protocol and the padlock symbol in the URL bar.</p>
+
+      <p>You can also contribute to safeguarding your personal data by following the guidelines below:</p>
+      <ul>
+        <li>Select a strong password, incorporating special characters and numbers, and change it regularly. If you need to note it down, ensure it is kept in a secure place.</li>
+        <li>Ensure privacy when entering login details, preventing others from viewing them.</li>
+        <li>Close your browser and end your session each time you log off. If possible, clear the history and cached data of visited websites.</li>
+        <li>Never disclose your account details to anyone.</li>
+        <li>Keep your software up to date with the latest security upgrades and software protection.</li>
+      </ul>
+      <p>To maximize the benefits of our services, please ensure that your personal data, including your email address, is accurate and up to date. You can achieve this by logging into our secure website. Once we have received your registration, we will retain your personal data, excluding the automatic information mentioned earlier.</p>
+
+      <h2>6. For how long do we retain your personal data?</h2>
+      <p>Regarding information concerning your financial transactions, we will retain it for the duration required by relevant tax and revenue legislation. As for your personal profile and membership data, we will retain it while you remain an active free or paid member and for a period of up to 7 years after you cease your membership activity. For all other instances, we will erase or anonymize your data once it is no longer necessary for the purpose for which we obtained it.</p>
+
+      <p>We may allow third-party service providers to process your personal data when it is necessary for the services they provide to us. Nonetheless, we always strive to maintain the highest standards of privacy protection. It is important to know that our Privacy Policy does not apply to third-party websites displaying our online advertisements or linked third-party websites that we do not operate or control.</p>
+
+      <p>We may share your information with selected third parties, including:</p>
+      <ul>
+        <li>Business partners, suppliers, and sub-contractors to fulfill contracts between us and them or you.</li>
+        <li>Advertisers and advertising networks that use the data to serve relevant advertisements to you and others.</li>
+        <li>Analytics and search engine providers that assist us in improving and optimizing our website.</li>
+        <li>Credit reference agencies, if assessing your credit score is a condition for us entering into a contract with you.</li>
+        <li>In case of selling or acquiring any business or assets, we may disclose your personal data to the prospective seller or buyer of such assets.</li>
+        <li>If substantially all of our assets are acquired by a third party, personal data held by us about our customers will be transferred as one of the assets.</li>
+        <li>If required to comply with legal obligations or enforce agreements between us and you, or to protect our rights, property, or safety, we may disclose your personal data. This may involve sharing information with other companies and organizations for purposes like fraud protection and credit risk reduction.</li>
+      </ul>
+
+      <h2>7. How to withdraw your consent, including marketing opt-out?</h2>
+      <p>You have the right to withdraw your consent for data processing at any time. Such withdrawal will not affect the lawfulness of processing based on consent before its withdrawal.</p>
+
+      <p>If you wish to opt-out of marketing purposes, you can instruct us not to process your personal information for marketing activities. Typically, you will either explicitly agree to our use of your personal information for marketing purposes in advance, or we will provide you with an opportunity to opt-in to such use. To unsubscribe from our emails, you can click on the &quot;unsubscribe&quot; link at the bottom of the email.</p>
+
+      <p>If you want to withdraw your consent or opt-out of marketing communications at any time, you can also contact us at privacy@frakt.com. We will promptly address your request and ensure that your preferences are respected.</p>
+
+      <h2>8. Do we conduct profiling and automated decision making?</h2>
+      <p>To better understand how visitors use our website, we utilize analytics software and online tools to collect information about your interactions with our site, including anonymous data from the forms you fill out. This data helps us analyze visitor behavior, track visits across multiple devices, administer our site, and manage your account. For more information about Google Analytics, please refer to our Cookie Notice.</p>
+
+      <p>However, aside from the data automatically collected through Google Analytics, we do not engage in any other profiling or automated decision-making processes concerning your information when you visit our site.</p>
+
+      <h2>9. Monitoring</h2>
+      <p>Subject to applicable laws, we may record and monitor communications with you to ensure adherence to our legal and regulatory obligations, as well as our internal policies. This monitoring may involve recording telephone conversations for the mentioned purposes.</p>
+
+      <h2>10. Your Rights</h2>
+      <p>You have various rights regarding your information. These rights include the ability to:</p>
+      <ul>
+        <li>Access your information</li>
+        <li>Withdraw your consent to the processing of your information at any time</li>
+        <li>Request changes to the information we hold to ensure accuracy and currency</li>
+        <li>Request the deletion or erasure of your information (the right to be forgotten)</li>
+        <li>Request the cessation or restriction of our processing of your information</li>
+        <li>Object to our processing of your information</li>
+        <li>Avoid automated decision-making</li>
+        <li>Request the transfer of some of the information we hold about you (known as data portability)</li>
+      </ul>
+      <p>To exercise any of these rights, please contact us at privacy@frakt.com. Please note that, for record-keeping and audit purposes, we may retain an archived copy of any information provided to us.</p>
+
+      <p>If you wish to obtain a copy of the information we hold about you, please send your request in writing to privacy@frakt.com.</p>
+
+      <h2>11. Children&apos;s Privacy</h2>
+      <p>FRAKTO platform is not intended for individuals under the age of 18. We do not knowingly collect personal information from children. If you believe a child has provided us with personal data, please contact us, and we will promptly remove it from our records.</p>
+
+      <h2>12. Third-Party Websites</h2>
+      <p>The website includes links to external websites. We do not assume responsibility for the privacy policies or practices of these third-party websites.</p>
+
+      <h2>13. Social media</h2>
+      <p>We maintain channels, pages, and accounts on various social media platforms to inform, assist, and engage with our customers. We monitor and keep a record of comments and posts related to us on these channels to enhance our services.</p>
+
+      <p>Please note that we are not liable for any information posted on these sites by others, except for the information we have posted ourselves. Our endorsement does not extend to the social media sites themselves or any information shared by third parties on these platforms.</p>
+
+      <h2>14. International data transfers</h2>
+      <p>The information we collect may be stored, processed, and transferred between any countries where we operate, allowing us to use the information in accordance with this Privacy Policy. Any transfers of personal data will adhere to applicable Data Protection Laws.</p>
+
+      <p>Furthermore, personal information that you submit for publication on the website will be made available on the internet and may be accessible worldwide. We cannot control the use or misuse of such information by others. By using our services, you expressly consent to such transfers of personal information.</p>
+
+      <h2>15. Changes to this privacy policy</h2>
+      <p>We reserve the right to update this Privacy Policy at our sole discretion, so we advise you to review it periodically.</p>
+
+      <h2>16. Contact</h2>
+      <p>If you have any inquiries, concerns, or complaints regarding the processing of your personal information, please reach out to us at privacy@fraktc.com.</p>
+    </div>
+  );
+}
